2c06d545ec34ee01d4e003e4a4b1787439981410
M Umer <mumertrade8@gmail.com>
2025-04-17 23:24:41 +0500

    Opened 500 port, little bit changes in script

--- templates/start_app.sh
+++ templates/start_app.sh
@@ -3,28 +3,31 @@
 # Exit script on any error
 set -e
 
-# Go to home directory
-cd /home/ubuntu || exit 1
+# # Go to home directory
+cd /home/ubuntu 
 
 # Update and install necessary packages
 sudo apt update -y
 sudo apt install -y python3 python3-pip git
+sudo apt install -y python3.12-venv
 
 # Clone the GitHub repo if it doesn't already exist
-if [ ! -d "python-mysql-db-proj-1" ]; then
-  git clone https://github.com/rahulwagh/python-mysql-db-proj-1.git
+if [ ! -d "simple-flask-app-for-cloud" ]; then
+  git clone https://github.com/Muhammad1umer-tech/simple-flask-app-for-cloud.git
 else
   echo "Repo already exists, skipping clone."
 fi
 
 # Give it a moment (optional)
-sleep 20
+sleep 5
 
-cd python-mysql-db-proj-1 || exit 1
+cd simple-flask-app-for-cloud || exit 1
 
 # Create a virtual environment
 python3 -m venv venv
+
 sleep 5
+
 # Activate the environment
 source venv/bin/activate
 
